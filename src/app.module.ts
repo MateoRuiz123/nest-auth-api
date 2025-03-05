8f902c9111954d2bb2120dc7490ab9f6f770fc5e
Mateo Ruiz <teiito.ruiz13@gmail.com>
2025-03-05 15:24:26 -0500

    Add TypeORM integration and initial Auth and Users modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { User } from './users/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost', // Cambia si es necesario
+      port: 5432, // Puerto por defecto de Postgres
+      username: 'postgres', // Tu usuario de Postgres
+      password: '14122004', // Tu password de Postgres
+      database: 'nestauth', // Nombre de la BD (asegúrate de que exista o usa synchronize)
+      entities: [User],
+      synchronize: true, // Sólo para desarrollo; en producción se recomienda migraciones
+    }),
+    AuthModule,
+    UsersModule,
+  ],
+  UsersModule,
+  AuthModule,
   controllers: [AppController],
   providers: [AppService],
 })
