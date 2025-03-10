97a2de2e019d2fcd6886d0819312716c009b86e9
Mateo Ruiz <teiito.ruiz13@gmail.com>
2025-03-10 16:17:51 -0500

    se activan los cors

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:3000', // Dominio de tu front
-    credentials: true, // Permite el envío de cookies
-  });
-  await app.listen(process.env.PORT ?? 8080);
+  app.enableCors();
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
