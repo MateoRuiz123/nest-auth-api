ff6c89867ca9919cf0d309104983137a955ba55a
Mateo Ruiz <teiito.ruiz13@gmail.com>
2025-03-27 18:54:25 -0500

    Update database connection settings for TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,14 +12,24 @@ import { Task } from './tasks/task.entity';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
+      host: 'centerbeam.proxy.rlwy.net',
+      port: 44114,
       username: 'postgres',
-      password: '14122004',
+      password: 'pgSxzkVMzezwcVBhEVDOdLceBRkcoFiW',
       database: 'nestauth', // Nombre de la BD (asegúrate de que exista o usa synchronize)
       entities: [User, Task],
       synchronize: true, // Sólo para desarrollo; en producción se recomienda migraciones
     }),
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: 'localhost',
+    //   port: 5432,
+    //   username: 'postgres',
+    //   password: '14122004',
+    //   database: 'nestauth', // Nombre de la BD (asegúrate de que exista o usa synchronize)
+    //   entities: [User, Task],
+    //   synchronize: true, // Sólo para desarrollo; en producción se recomienda migraciones
+    // }),
     UsersModule,
     AuthModule,
     TasksModule,
